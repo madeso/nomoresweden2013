320c92fac78773236cc9e5a7a9b45a026cfe09f5
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-19 16:12:53 +0200

    basic structure up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 This is my entry for nomoresweden 2013.
+
+Created by:
+=============
+* Code(gpl) by Gustav Jansson
+* Public font, PressStart2P(OFL) by Cody \'CodeMan38\' Boisclair http://openfontlibrary.org/en/font/press-start-2p
---
+++ main.lua
@@ -0,0 +1,104 @@
+
+
+-- Aliases
+local Draw = love.graphics.draw
+local PauseAllAudio = love.audio.pause
+local Print = love.graphics.print
+
+function Play(x)
+	x:rewind()
+	love.audio.play(x)
+end
+
+function Img(p)
+	local r
+	r = love.graphics.newImage(p)
+	return r
+end
+
+function Music(p)
+	local s
+	s = love.audio.newSource(p, "stream")
+	return s
+end
+
+function Sfx(p)
+	local s
+	s = love.audio.newSource(p, "static")
+	return s
+end
+
+function love.load()
+	love.graphics.setFont(love.graphics.newFont("PressStart2P.ttf", 20))
+	math.randomseed( tonumber(tostring(os.time()):reverse():sub(1,6)) )
+end
+
+function BeginPrint()
+	if worldindex == 1 then
+		love.graphics.setColor(0, 0, 0)
+	end
+end
+
+function EndPrint()
+	love.graphics.setColor(255, 255, 255)
+end
+
+--------------------------------------------------
+
+STATETITLE = 0
+
+
+state = STATETITLE
+--------------------------------------------------
+
+function title_setup()
+end
+
+function title_draw()
+	BeginPrint()
+		Print("The game", 300, 300)
+	EndPrint()
+end
+
+function title_onkey(key)
+end
+
+function title_update(dt)
+end
+
+---------------------------------------------------
+
+function Setup()
+	if state == STATETITLE then title_setup()
+	else
+		print("unknown gamestate " .. state)
+	end
+end
+
+function love.draw()
+	if state == STATETITLE then title_draw()
+	else
+		BeginPrint()
+		love.graphics.print("unknown gamestate " .. state, 400, 300)
+		EndPrint()
+	end
+end
+
+function love.keyreleased(key)
+	if key == "escape" then
+		love.event.push("quit")   -- actually causes the app to quit
+	end
+	
+	if state == STATETITLE then title_onkey(key)
+	else
+		print("unknown gamestate " .. state)
+	end
+end
+
+function love.update(dt)
+	if state == STATETITLE then title_update(dt)
+	else
+		print("unknown gamestate " .. state)
+	end
+end
+
