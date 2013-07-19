2eecac349c9b8cbd9df47ad83d5af91f2415bac1
Gustav <sir.gustav.the.coder@gmail.com>
2013-07-19 19:12:49 +0200

    seond player

--- main.lua
+++ main.lua
@@ -83,10 +83,17 @@ local gfxbox = Img("assets/box.png")
 
 --------------------------------------------------
 
-local p1left = "left"
-local p1right = "right"
-local p1down = "down"
-local p1up = "up"
+local p1left = "a"
+local p1right = "d"
+local p1down = "s"
+local p1up = "w"
+local p1action = "q"
+
+local p2left = "left"
+local p2right = "right"
+local p2down = "down"
+local p2up = "up"
+local p2action = "return"
 
 --------------------------------------------------
 
@@ -114,34 +121,42 @@ end
 
 MODTITLES = {"Shape", "Color", "Hull", "Weapon", "Flaw"}
 MODDESC = {{"The BOX is a easy shape to furnish.", "The TRIANGLE blends standard ship design and powerful attack vectors.", "The CIRCLE shape is powerful, the enemy doesn't know what way you're facing."}, {"RED is the color of a angry romulan warbird.", "GREEN is the color of peace.", "BLUE is a kinda nice color."}, {"WOOD may not be the best material to build a spaceship of but it's cheap and pretty durable", "IRON is the material most cruisers are made of, but they are slow because of it.", "GLASS is really fragile but also really light"}, {"The ROCKETS are powerful and reliable for a first craft owner.", "The ATOM BOMB is a truly devestating weapon but it also takes time to reload.", "The SLINGSHOT might not be the most dangerous weapon but it takes no time to reload"}, {"FRAGILE: The architect was lazy and didn't really consider structural integrity.", "WEAK ENGINE: The mechanics has made a engine that is weak, it might even be too weak.", "HEAT PROBLEM: The engineers forgot to install proper cooling. The heat from the engine and guns will be a problem and there is no excuse for that."}}
-p1data = {0,0,0,0,0}
+
 	--------------------------------------------------
 	local p1place = 0
+	local p1data = {0,0,0,0,0}
+	local p2place = 0
+	local p2data = {0,0,0,0,0}
 	
 	function craft_setup()
-		p1place = 0
+		p2place = 0
 	end
 
-	function craft_draw()
-		
+	local TEXTWIDTH = 300
+
+	function craft_draw_player(data,place,x,d,tx,align)
 		for i=0, 4 do
 			local y = 10+i*110
-			SetColor(p1data[i+1])
-			Draw(gfxbox, 10, y)
-			if p1place == i then
-				SetColor(mod3(p1data[p1place+1]-1))
-				Draw(gfxbox, 120, y)
-				SetColor(mod3(p1data[p1place+1]+1))
-				Draw(gfxbox, 230, y)
+			SetColor(data[i+1])
+			Draw(gfxbox, x, y)
+			if place == i then
+				SetColor(mod3(data[place+1]-1))
+				Draw(gfxbox, x+d*110, y)
+				SetColor(mod3(data[place+1]+1))
+				Draw(gfxbox, x+d*110*2, y)
 			end
-				SetDefaultColor()
+			SetDefaultColor()
 		end
 		
 		BeginPrint()
-			love.graphics.printf(MODTITLES[p1place+1] .. ":", 120, 100, 200, "left")
-			love.graphics.printf(MODDESC[p1place+1][p1data[p1place+1]+1], 120, 120, 200, "left")
-	
+			love.graphics.printf(MODTITLES[place+1] .. ":", tx, 100, TEXTWIDTH, align)
+			love.graphics.printf(MODDESC[place+1][data[place+1]+1], tx, 120, TEXTWIDTH, align)
 		EndPrint()
+	end
+
+	function craft_draw()
+		craft_draw_player(p1data,p1place,10,1,120,"left")
+		craft_draw_player(p2data,p2place,690,-1,680-TEXTWIDTH,"right")
 	end
 
 	function craft_onkey(key, down)
@@ -163,6 +178,25 @@ p1data = {0,0,0,0,0}
 			end
 			if key == p1right then
 				p1data[p1place+1] = mod3(p1data[p1place+1]+1)
+			end
+			
+			if key == p2up then
+				p2place = p2place - 1
+			end
+			if key == p2down then
+				p2place = p2place + 1
+			end
+			if p2place == -1 then
+				p2place = 4
+			end
+			if p2place == 5 then
+				p2place = 0
+			end
+			if key == p2left then
+				p2data[p2place+1] = mod3(p2data[p2place+1]+1)
+			end
+			if key == p2right then
+				p2data[p2place+1] = mod3(p2data[p2place+1]-1)
 			end
 		end
 	end
